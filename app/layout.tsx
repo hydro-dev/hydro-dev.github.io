ca41bc86cf31523aaa13b826a3375f44eb76adac
undefined <i@undefined.moe>
2025-03-08 18:35:59 +0800

    remove all vuepress files

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,15 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
+const analytics = `var _paq=window._paq=[['trackPageView'],['enableLinkTracking']];(function(){var u="//analytics.hydro.ac/";_paq.push(['setTrackerUrl',u+'mt']);_paq.push(['setSiteId','2']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.async=true;g.src=u+'m.js';s.parentNode.insertBefore(g,s);})();`;
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.png" />
+        <meta name="theme-color" content="#ffeded" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider
           search={{
@@ -21,6 +27,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         >
           {children}
         </RootProvider>
+        <script dangerouslySetInnerHTML={{ __html: analytics }} />
       </body>
     </html>
   );
