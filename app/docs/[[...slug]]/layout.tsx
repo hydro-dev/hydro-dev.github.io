60e01f20096d34bd7eaab5344ceaca0a45146f82
undefined <i@undefined.moe>
2025-09-16 00:31:57 +0800

    fix types

--- app/docs/[[...slug]]/layout.tsx
+++ app/docs/[[...slug]]/layout.tsx
@@ -10,7 +10,7 @@ const maps = {
   'Tools': <GithubInfo owner="hydro-dev" repo="xcpc-tools" />,
 };
 
-export default async function Layout({ children, params }: { children: ReactNode, params: Promise<{ slug: string[] }> }) {
+export default async function Layout({ children, params }: { children: ReactNode, params: Promise<{ slug?: string[] }> }) {
   const { slug } = await params;
   return (
     <DocsLayout
